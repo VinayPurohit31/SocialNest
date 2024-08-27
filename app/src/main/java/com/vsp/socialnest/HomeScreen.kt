0db0780ebdc61f5c94884c0eb04dd6afa3b67b7d
VinayPurohit31 <vinaypurohit31@gmail.com>
2024-08-27 13:20:42 +0530

    home screen 5 Fregment Created

--- app/src/main/java/com/vsp/socialnest/HomeScreen.kt
+++ app/src/main/java/com/vsp/socialnest/HomeScreen.kt
@@ -24,11 +24,7 @@ class HomeScreen : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_home_screen)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
+
        // setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
